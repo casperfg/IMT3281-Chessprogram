eaace041deb28da97ce23e7e4a07f3ce43354a2e
Mads Olsen Nekkøy <madsone@stud.ntnu.no>
2020-11-09 13:05:16 +0100

    changes to internationalization

--- src/main/ChessProgram.java
+++ src/main/ChessProgram.java
@@ -38,15 +38,16 @@ import javax.swing.*;
 
 public class ChessProgram extends Application {
 
-
-    Locale currentLocale = setLanguage("en", "UK");
-    ResourceBundle messages = ResourceBundle.getBundle("languages/MessagesBundle", currentLocale); //fetches resource bundle.
+    String defaultLanguage = "en";
+    String defaultCountry = "UK";
+    Locale currentLocale = setLanguage(defaultLanguage, defaultCountry); //sets default language to english.
 
 
 
 
     @Override
     public void start(Stage primaryStage) {
+        ResourceBundle messages = ResourceBundle.getBundle("languages/MessagesBundle", currentLocale); //fetches resource bundle.
 
         System.out.println(currentLocale);
         final int size = 8;
@@ -71,8 +72,16 @@ public class ChessProgram extends Application {
 
 //================== Language Item event handler ==================
         Norwegian.setOnAction(e -> {
-            Locale currentLocale = setLanguage("no", "NO");
+            currentLocale = setLanguage("no", "NO");
+            start(primaryStage);
         });
+
+        English.setOnAction(e -> {
+            currentLocale = setLanguage("en", "UK");
+            start(primaryStage);
+        });
+
+
 
 
         langSubMenu.getItems().addAll(Norwegian, English); //adds item to language
@@ -83,7 +92,7 @@ public class ChessProgram extends Application {
         Menu help = new Menu(messages.getString("Help"));
         MenuBar menubar = new MenuBar();
         menubar.getMenus().addAll(file, settings, help);
-        
+
         GridPane chessboard = createChessBoard();
 
 
@@ -149,12 +158,6 @@ public class ChessProgram extends Application {
         }
         return currentLocale; //return locale object.
     }
-
-    public Locale getLanguage(){
-
-        return  null;
-    }
-
 
 
     public ImageView setIcon(Image flagIcon){ //gets an image as parameter and adds to imageView
