41a0a69096ae6435a26732ef86a9a81092ebad2b
Håkon Muggerud <haakontm@stud.ntnu.no>
2020-11-20 12:10:16 +0100

    Refactor piece classes

--- src/main/pieces/King.java
+++ src/main/pieces/King.java
@@ -1,27 +1,25 @@
 package main.pieces;
 
 import main.Chessboard;
-
 import javafx.scene.image.Image;
-public class King extends Piece{
+
+public class King extends Piece {
+
     // movevector same as queen, but not reating vector.
     public boolean castled = false;
     public Piece towerPiece;
     public int[][] moveVector = new int[][]{{0,1},{0, -1},{1,0},{-1, 0}, {1,-1},{1, 1},{-1,1},{-1, -1}};
-    public King(boolean color){
-        super(color, "king", 'k');
-    }
 
-    public King(){
+    public King() {}
 
-    }
-    public void setIcon(){
+    public void setIcon() {
         if(color){
             icon = new Image(getClass().getResourceAsStream("/images/white_king.png"));
         }else{
             icon = new Image(getClass().getResourceAsStream("/images/black_king.png"));
         }
     }
+
     public void possible(Chessboard board) {
         nonRepeat(moveVector, board);
 
@@ -34,10 +32,12 @@ public class King extends Piece{
             }
         }
     }
-    public boolean canCastle(Chessboard board){
+
+    public boolean canCastle(Chessboard board) {
         return (!board.whiteCastle && color) || (!board.blackCastle && !color);
     }
-    public boolean shortCastle(Chessboard board){ // if shortCastle is possible.
+
+    public boolean shortCastle(Chessboard board) { // if shortCastle is possible.
         int y = color? 7 : 0;
         int x = 7;
         if(!board.blankSq(x, y)) {
@@ -52,7 +52,8 @@ public class King extends Piece{
         }
         return false;
     }
-    public boolean longCastle(Chessboard board){
+
+    public boolean longCastle(Chessboard board) {
         int y = color? 7 : 0; // position of tower
         int x = 0;
         if(!board.blankSq(x, y)) {
--- src/main/pieces/Rook.java
+++ src/main/pieces/Rook.java
@@ -4,22 +4,23 @@ import javafx.scene.image.Image;
 import main.Chessboard;
 
 public class Rook extends Piece {
+
     public int[][] moveVector = new int[][]{{0,1},{0, -1},{1,0},{-1, 0}};
-    public Rook(boolean color){
+
+    public Rook(boolean color) {
         super(color, "rook", 'r');
     }
 
-    public Rook(){
+    public Rook() {}
 
-    }
-
-    public void setIcon(){
+    public void setIcon() {
         if(color){
             icon = new Image(getClass().getResourceAsStream("/images/white_rook.png"));
         }else{
             icon = new Image(getClass().getResourceAsStream("/images/black_rook.png"));
         }
     }
+
     public void possible(Chessboard board) {
         repeat(moveVector, board);
     }
