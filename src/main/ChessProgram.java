266d50725be4c4eb25d9bed054bd01d463b769ad
Mads Olsen Nekkøy <madsone@stud.ntnu.no>
2020-11-20 12:12:27 +0100

    bugfix for win game

--- src/main/ChessProgram.java
+++ src/main/ChessProgram.java
@@ -597,7 +597,7 @@ public class ChessProgram extends Application {
         if (controller.chessboard.checkStaleMate()){
             info.appendText("\n"+messages.getString("Stalemate"));
             winColor();
-        } else if (controller.chessboard.mateCheck() || !controller.engineRunning){
+        } else if (controller.chessboard.mateCheck() || (!controller.engineRunning && controller.game.contains("e"))){
             info.appendText("\n"+messages.getString("Mate"));
             winColor();
             controller.chessboard.isMate = false;
